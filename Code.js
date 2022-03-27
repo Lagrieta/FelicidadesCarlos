b7abed60cda53db283693416d6b7fb5548cff0ba
Lagrieta <lagriet@hotmail.com>
2022-03-27 16:51:42 -0600

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PRELOAD";
+const accessCode2 = "MATTER";
+const accessCode3 = "CARLOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("Clave 1", 100,50)
+    fill("lightblue")
+    text("Cómo se le llama a la función de carga de imágenes", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Clave 2", 700,150)
+    fill("lightblue")
+    text("¿Qué librería nos ayuda a dar materia?", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("Clave 3", 100,250)
+    fill("lightblue")
+text("¿Cómo se llama mi mejor alumno?", 100,270)
+
+}
